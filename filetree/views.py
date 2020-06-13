17d687572c41223a42fb04e4ed9314915baf90de
Chelsea White <chewh115@gmail.com>
2020-06-12 21:29:38 -0400

    Updates database to have data + display on index.html

--- filetree/views.py
+++ filetree/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Files
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {}
+    context['files'] = Files.objects.all()
+    return render(request, 'index.html', context)
